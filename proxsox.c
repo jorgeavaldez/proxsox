5126181b200eefc3952abd60138d97f5a1f2ef7f
Jorge Valdez <valdez.a.jorge@gmail.com>
2018-04-25 15:20:47 -0500

    attempt 1 at socket server

--- proxsox.c
+++ proxsox.c
@@ -9,6 +9,58 @@
 // open ssl bc smu uses https
 #include <openssl/ssl.h>
 
+#define PORT 8080
+
+SSL_CTX *ssl_ctx;
+
+void createServer() {
+  int serverfd, new_sock, valread;
+  struct sockaddr_in address;
+
+  int opt = 1;
+  int addrlen = sizeof(address);
+
+  char buf[2056] = {0};
+  char *server_res;
+
+  // create the socket
+  if ((serverfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    printf("failed creating server socket");
+    exit(1);
+  }
+
+  // set options to bind to the port
+  if (setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                 &opt, sizeof(opt))) {
+    printf("setsockopt failed");
+    exit(1);
+  }
+
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(PORT);
+
+  if (bind(serverfd, (struct sockaddr *)&address,
+           sizeof(address)) < 0) {
+    printf("binding to port failed");
+    exit(1);
+  }
+
+  if (listen(serverfd, 3) < 0) {
+    printf("listen failed");
+    exit(1);
+  }
+
+  if ((new_sock = accept(serverfd, (struct sockaddr *)&address,
+                           (socklen_t*)&addrlen)) < 0) {
+    printf("accept failed");
+    exit(1);
+  }
+
+  valread = read(new_sock, buf, 2056);
+  printf("%s\n", buf);
+}
+
 void makeSSLRequest () {
   struct addrinfo hints, *res;
   int sockfd;
@@ -37,9 +89,6 @@ void makeSSLRequest () {
   // set up openssl
   SSL_load_error_strings();
   SSL_library_init();
-
-  // ssl_ctx needs to be a global var
-  SSL_CTX *ssl_ctx = SSL_CTX_new(SSLv23_client_method());
 
   // create an ssl connection and attach it to the socket
   SSL *conn = SSL_new(ssl_ctx);
@@ -70,7 +119,10 @@ void makeSSLRequest () {
 }
 
 int main(void) {
-  printf("Let's go!!!\n");
-  makeSSLRequest();
+  // ssl_ctx needs to be a global var
+  ssl_ctx = SSL_CTX_new(SSLv23_client_method());
+
+  //makeSSLRequest();
+  createServer();
   return 0;
 }
